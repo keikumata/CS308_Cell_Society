9c2ea5aeef0df2158386c9bcb3daa74f046c8fb4
nicsabNTS9 <nts9@duke.edu>
2015-01-27 19:41:39 -0500

    Made a variable size GridPane.
    Made a variable size GridPane.
    made global var boardSizeConstant as a reminder this should be changed
    later

---
+++ src/cellsociety_team05/Game.java
@@ -0,0 +1,56 @@
+package cellsociety_team05;
+
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+// http://docs.oracle.com/javafx/2/layout/builtin_layouts.htm
+
+public class Game extends Application {
+
+	int boardSizeConstant = 69;
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+		
+		
+		Scene scene = new Scene(setUpGridPane(boardSizeConstant), 769, 769);
+		
+		stage.setTitle("Grid Pane");
+		stage.setScene(scene);
+		stage.show();
+		
+	}
+	
+	GridPane setUpGridPane(int boardSize) {
+		GridPane grid = new GridPane();
+		grid.setGridLinesVisible(true);
+
+		for (int i = 0; i < boardSize; i++) {
+			
+			ColumnConstraints c = new ColumnConstraints();
+			c.setPercentWidth(100.0 / boardSize);
+			
+			RowConstraints r = new RowConstraints();
+			r.setPercentHeight(100.0 / boardSize);
+			
+			grid.getColumnConstraints().add(c);
+			grid.getRowConstraints().add(r);
+		}
+		
+		
+		return grid;
+	}
+	
+	
+
+}
