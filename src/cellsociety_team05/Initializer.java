c09a316abe4d2be5e480465c0bf7049f590be30a
keikumata <ky52@duke.edu>
2015-01-28 17:48:24 -0500

    made minor changes to initializer and SimData

--- src/cellsociety_team05/Initializer.java
+++ src/cellsociety_team05/Initializer.java
@@ -1,10 +1,7 @@
 package cellsociety_team05;
 
-
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.xml.sax.helpers.DefaultHandler;
 import org.xml.sax.helpers.XMLReaderFactory;
@@ -15,7 +12,7 @@ import org.xml.sax.XMLReader;
 /**
  * 
  * @author Kei Yoshikoshi
- * Reads the XML File and stores them in a Map
+ * Reads the XML File and stores them in separate variables
  *
  */
 public class Initializer extends DefaultHandler {
@@ -24,21 +21,15 @@ public class Initializer extends DefaultHandler {
 	private int size;
 	private int delay;
 	private ArrayList<Integer> paramsList = new ArrayList<>();
-	//	private Map<String, ArrayList<Integer>> myMap = new HashMap<>();
 
 	public void startDocument() {
-		System.out.println("Starting\n");
 	}
 	public void endDocument() {
-		System.out.println("\nend parsing document..");
 	}
 	public void startElement(String nameSpaceURI, String localName, String qName, Attributes atts) {
-		System.out.println("<"+qName+">");
 		tag = qName;
-
 	}
 	public void endElement(String namespaceURI, String localName, String qName) {
-		System.out.println("</"+qName+">");
 	}
 	public void characters(char[] ch, int start, int length) {
 		String num = new String(ch, start, length).trim();
@@ -56,10 +47,15 @@ public class Initializer extends DefaultHandler {
 		}
 		else if (tag.equals("param")) {
 			paramsList.add(Integer.parseInt(num));
+			System.out.println(paramsList);
 		}
 	}
-	public static void main(String[] args) throws SAXException {
-		XMLReader xml = XMLReaderFactory.createXMLReader();
+	public Sim setup(){
+		System.out.println(this.type);
+		return new Sim(type, size, delay, paramsList);
+	}
+	public void readXML() throws SAXException {
+    	XMLReader xml = XMLReaderFactory.createXMLReader();
 		xml.setContentHandler(new Initializer());
 		try {
 			xml.parse("/Users/keiyoshikoshi/Documents/CS308/workspace/cellsociety_team05/src/cellsociety_team05/example.xml");
@@ -67,9 +63,17 @@ public class Initializer extends DefaultHandler {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
-
-	private Sim setup(){
-		return new Sim(type, size, delay, paramsList);
+    }
+	public static void main(String[] args) throws SAXException {
+		XMLReader xml = XMLReaderFactory.createXMLReader();
+		Initializer init = new Initializer();
+		xml.setContentHandler(new Initializer());
+		try {
+			xml.parse("/Users/keiyoshikoshi/Documents/CS308/workspace/cellsociety_team05/src/cellsociety_team05/example.xml");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		init.setup();
 	}
 }
--- src/cellsociety_team05/Master.java
+++ src/cellsociety_team05/Master.java
@@ -18,6 +18,7 @@ public class Master {
 
     public Scene init (Stage s) throws Exception {
         Initializer initializer = new Initializer();
+        initializer.readXML();
         Sim sim = initializer.setup();
         SceneUpdater updater = new SceneUpdater();
         return updater.newScene(sim.getData());
@@ -31,15 +32,5 @@ public class Master {
         KeyFrame frame = addKeyFrame(NUM_FRAMES_PER_SECOND);
         animation.getKeyFrames().add(frame);
         animation.play();
-    }
-    public void readXML() throws SAXException {
-    	XMLReader xml = XMLReaderFactory.createXMLReader();
-		xml.setContentHandler(new Initializer());
-		try {
-			xml.parse("/Users/keiyoshikoshi/Documents/CS308/workspace/cellsociety_team05/src/cellsociety_team05/example.xml");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
     }
 }
