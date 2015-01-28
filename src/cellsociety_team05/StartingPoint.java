03480322a2b613126a152ac05442004d687c279d
nicsabNTS9 <nts9@duke.edu>
2015-01-27 19:14:35 -0500

    im just gna start here for now

---
+++ src/cellsociety_team05/StartingPoint.java
@@ -0,0 +1,27 @@
+package cellsociety_team05;
+
+
+import javafx.application.Application;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class StartingPoint extends Application {
+
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		
+		GridPane grid = new GridPane();
+		grid.setGridLinesVisible(true);
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+
+}
