975ee0ba7a975aada6ed76c09492485a004158bb
keikumata <ky52@duke.edu>
2015-02-06 15:24:18 -0500

    pushing a small change so that i can fix this weird pull error

--- src/cellsociety_team05/Main.java
+++ src/cellsociety_team05/Main.java
@@ -1,27 +1,26 @@
 // This entire file is part of my masterpiece.
 // Mengchao Feng
 package cellsociety_team05;
-	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
-    //private Snake myGame;
-    private Master master;
-    
+	// private Snake myGame;
+	private Master master;
+
 	@Override
 	public void start(Stage s) {
 		try {
-		    master = new Master();
-		    master.init(s);
-	        s.show();
-//	        master.play();
-		} catch(Exception e) {
+			master = new Master();
+			master.init(s);
+			s.show();
+			// master.play();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
