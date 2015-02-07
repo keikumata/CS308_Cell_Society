1bb9649b316c3cea616a00a36ea087717782bfd7
keikumata <ky52@duke.edu>
2015-02-07 02:08:44 -0500

    added XMLNotFoundException and gave it default simulation values

--- src/error/XMLNotFoundException.java
+++ src/error/XMLNotFoundException.java
@@ -10,10 +10,13 @@ import simulations.Sim;
 public class XMLNotFoundException extends FileNotFoundException {
 	 // for serialization
     private static final long serialVersionUID = 1L;
-    private int type = 1;
-    private int length = 30;
-    private int fps = 10;
-    private int sides = 4;
+    private static final int SIM_TYPE = 1;
+    private static final int SIM_LENGTH = 30;
+    private static final int SIM_FPS = 10;
+    private static final int SIM_SIDES = 4;
+    private static final int SIM_PARAM1 = 50;
+    private static final int SIM_PARAM2 = 30;
+    private static final int SIM_PARAM3 = 70;
     private List<Integer> params = new ArrayList<>();
     
 	public XMLNotFoundException(String message) {
@@ -21,10 +24,10 @@ public class XMLNotFoundException extends FileNotFoundException {
 	}
 	
 	public Sim returnDefaultSim() {
-		params.add(50);
-		params.add(30);
-		params.add(80);
-		return new Schelling(type,length,fps,sides,params);
+		params.add(SIM_PARAM1);
+		params.add(SIM_PARAM2);
+		params.add(SIM_PARAM3);
+		return new Schelling(SIM_TYPE,SIM_LENGTH,SIM_FPS,SIM_SIDES,params);
 	}
 	
 }
