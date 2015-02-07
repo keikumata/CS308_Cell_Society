657e58d372495a4140efb5507c2ed201c925f081
nicsabNTS9 <nts9@duke.edu>
2015-02-06 21:54:53 -0500

    slime finished
    perhaps

--- src/simulations/SlimeMold.java
+++ src/simulations/SlimeMold.java
@@ -2,24 +2,25 @@ package simulations;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 
 import utility.Pair;
 
 public class SlimeMold {
+	
+	public final int boardLength = 100;
 	
 	public class SlimeBox {
 		Pair coord;
 		boolean containsCell;
 		int pheromone;
 		int evaporationRate;
-		int diffusionRate;
 		
-		public SlimeBox(Pair coord, boolean containsCell, int pheromone,int evaporationRate, int diffusionRate) {
+		public SlimeBox(Pair coord, boolean containsCell, int pheromone, int evaporationRate) {
 			this.coord = coord;
 			this.containsCell = containsCell;
 			this.pheromone = pheromone;
 			this.evaporationRate = evaporationRate;
-			this.diffusionRate = diffusionRate;
 		}
 	}
 	
@@ -44,19 +45,31 @@ public class SlimeMold {
 		HashMap<Pair, SlimeBox> terrain = new HashMap<Pair, SlimeMold.SlimeBox>();
 		HashMap<Pair, SlimeCell> cells = new HashMap<Pair, SlimeMold.SlimeCell>();
 
-		HashMap<Pair, SlimeCell> cellsNext = updateCells(cells, terrain);
+		cells = updateCells(cells, terrain);
+		terrain = updateTerrain(cells, terrain);
 		
 		
-		HashMap<Pair, SlimeBox> terrainNext = updateTerrain(cellsNext, terrain);
 	}
 	
 	public HashMap<Pair, SlimeBox> updateTerrain(HashMap<Pair, SlimeCell> cellsNext, HashMap<Pair, SlimeBox> terrain) {
+
+		HashMap<Pair, SlimeBox> terrainNext = new HashMap<Pair, SlimeMold.SlimeBox>();
 		
+		for (Pair boxCoord : terrain.keySet()) {
+			SlimeBox box = terrain.get(boxCoord);
+			box.pheromone *= box.evaporationRate;
+			
+			if (cellsNext.get(boxCoord) != null) {
+				box.containsCell = true;
+			}
+			else {
+				box.containsCell = false;
+			}
+			
+			terrainNext.put(boxCoord, box);
+		}
 		
-		
-		
-		
-		return terrain;
+		return terrainNext;
 	}
 	
 	public HashMap<Pair, SlimeCell> updateCells(HashMap<Pair, SlimeCell> cells, HashMap<Pair, SlimeBox> terrain) {
@@ -73,27 +86,56 @@ public class SlimeMold {
 				cellsNext.put(cell.coord, cell);
 			}
 			else {
+				SlimeCell tempCell = new SlimeCell(cell.coord, cell.sniffThreshhold, cell.sniffAngle, cell.depositAmt, cell.facing);
 				cell.coord = whereNext(cell, terrain);
 				cellsNext.put(cell.coord, cell);
+				
+				if (tempCell.coord != cell.coord) {
+					SlimeBox tempBox = terrain.get(tempCell.coord);
+					tempBox.pheromone += tempCell.depositAmt;
+				}
 			}
 		}
+		
 		
 		return cellsNext;
 	}
 	
 	public Pair whereNext(SlimeCell cell, HashMap<Pair, SlimeBox> terrainXY) {
+
+		SlimeBox bCur = terrainXY.get(cell.coord);
 		
 		Pair forward = new Pair(cell.facing.r + cell.coord.r, cell.facing.c + cell.coord.c);
 		SlimeBox bForward = terrainXY.get(forward);
 		SlimeBox bAdj1, bAdj2;
 		
 		if (cell.facing.r == 0) {
-			bAdj1 = terrainXY.get(new Pair(forward.r - 1, forward.c));
-			bAdj2 = terrainXY.get(new Pair(forward.r + 1, forward.c));
+			if (!(forward.r - 1 < 0)) {
+				bAdj1 = terrainXY.get(new Pair(forward.r - 1, forward.c));
+			}
+			else {
+				bAdj1 = bCur;
+			}
+			if (!(forward.r + 1 > boardLength)) {
+				bAdj2 = terrainXY.get(new Pair(forward.r + 1, forward.c));
+			}
+			else {
+				bAdj2 = bCur;
+			}
 		}
 		else {
-			bAdj1 = terrainXY.get(new Pair(forward.r, forward.c + 1));
-			bAdj2 = terrainXY.get(new Pair(forward.r, forward.c - 1));
+			if (!(forward.c + 1 > boardLength)) {
+				bAdj1 = terrainXY.get(new Pair(forward.r, forward.c + 1));
+			}
+			else {
+				bAdj1 = bCur;
+			}
+			if (!(forward.c - 1 < 0)) {
+				bAdj2 = terrainXY.get(new Pair(forward.r, forward.c - 1));
+			}
+			else {
+				bAdj2 = bCur;
+			}
 		}
 		
 		ArrayList<SlimeBox> terrainAdj = new ArrayList<SlimeMold.SlimeBox>();
@@ -112,12 +154,14 @@ public class SlimeMold {
 			}
 		}
 		if (flag == true && bNext == bForward) {
-			bNext = terrainXY.get(cell.coord);
+			bNext = bCur;
 		}
-		
+		else if (bForward.pheromone == bAdj1.pheromone && bForward.pheromone == bAdj2.pheromone) {
+			Random random = new Random();
+			bNext = terrainAdj.get(random.nextInt(terrainAdj.size()));
+		}
 		return bNext.coord;
 	}
-
 }
 
 
