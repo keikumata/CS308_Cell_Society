f8852d751ddf496d7f3b2f5d43c2943ef7c58c2b
mf108 <mf108@duke.edu>
2015-02-09 00:28:26 -0500

    fire fixed

--- src/simulations/Fire.java
+++ src/simulations/Fire.java
@@ -19,21 +19,20 @@ public class Fire extends Sim{
 		super(sim, size, delay, cellSides, params);
 		fireProb=params.get(1);
 		fireProb=fireProb/100;
+        fireTotal=(int) (params.get(0)*calculateTotal()/100); 
+        burnedTotal=0;
+        forestTotal=(int) (calculateTotal()-burnedTotal-fireTotal);
 	}
 
 	public void nextGen(SceneUpdater updater){
+	    fireTotal=0;
 		int[][] tempMap = MapCopier.copyOfArray(map);
 		List<Integer> burningTrees = new ArrayList<Integer>();
-		fireTotal=0; burnedTotal=0; 
 		for (int row = 0; row < map.length; row++) {
 			for (int col = 0; col < map.length; col++) {
 				if(map[row][col]==1){
-					fireTotal++;
-					System.out.println("hey");
-					checkFire(row,col,tempMap,burningTrees);
+					checkFire(row,col,tempMap,burningTrees,updater);
 				}
-				
-				updater.updateScene(row,col,tempMap[row][col]);
 			}
 		}
 		System.out.println("Forest total is: " + forestTotal);
@@ -42,25 +41,31 @@ public class Fire extends Sim{
 		this.map = tempMap;
 	}
 
-	private void checkFire (int row, int col, int[][] tempMap, List<Integer> burningTrees) {
-		tempMap[row][col] = 2;
+	private void checkFire (int row, int col, int[][] tempMap, List<Integer> burningTrees, SceneUpdater updater) {
+        burnedTotal++;
+	    tempMap[row][col] = 2;
         neighbors=Neighborhood.getNeighbors(cellSides,col,4);
 		for (int[] neighbor:neighbors) {
 			if ((row+neighbor[0]>= 0 && row+neighbor[0] < map.length) && (col+neighbor[1] >= 0 && col+neighbor[1] < map.length) && map[row + neighbor[0]][col + neighbor[1]]==0) {
 				int treeIndex=(row+neighbor[0])*map.length+col+neighbor[1];
 				if(!burningTrees.contains(treeIndex)){
 					tempMap[row + neighbor[0]][col + neighbor[1]]=updateState(row + neighbor[0], col + neighbor[1], burningTrees);
+		            updater.updateScene(row + neighbor[0],col + neighbor[1],tempMap[row + neighbor[0]][col + neighbor[1]]);
 				}
 			}
 		}
+		updater.updateScene(row,col,2);
 	}
 
 	private int updateState(int row, int col, List<Integer> burningTrees) {
 		Random rand = new Random();
 		float fire = rand.nextFloat();
 		if(fire<fireProb){
+		    int index=row*map.length+col;
+		    System.out.println("tree"+index+"caught fire");
 			burningTrees.add(row*map.length+col);
-			burnedTotal++;
+            fireTotal++;
+            forestTotal--;
 			return 1;
 		}else{
 			return 0;
